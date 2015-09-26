143727a3e33e1917aeca16065bca97f7ed1b823b
Raj85 <rajeshramanujam.s@gmail.com>
2015-09-25 22:10:44 -0400

    UI file

---
+++ server.R
@@ -0,0 +1,30 @@
+library(tm)
+library(wordcloud)
+
+
+shinyServer(function(input, output) {
+  
+  output$cloud <- renderPlot({
+    input$submit
+    # read input and pre preprocessing
+    tmpText = data.frame(c(input$text1,input$text2))
+    ds = DataframeSource(tmpText)
+    corp = Corpus(ds)
+    corp = tm_map(corp,removePunctuation)
+    corp = tm_map(corp,tolower)
+    corp = tm_map(corp,removeNumbers)
+    corp = tm_map(corp, removeWords, stopwords("english"))
+    corp = tm_map(corp, PlainTextDocument)
+    term.matrix <- TermDocumentMatrix(corp)
+    term.matrix <- as.matrix(term.matrix)
+    colnames(term.matrix) <- c("1st Text","2nd Text")
+    par(mfrow=c(2,1))
+    set.seed(103)
+    comparison.cloud(term.matrix,max.words=100,random.order=FALSE, colors=brewer.pal(8,"Dark2"))
+    set.seed(786)
+    commonality.cloud(term.matrix,max.words=Inf,random.order=FALSE)
+    })
+  
+})
+
+
